b1d1f6e3ed6ed01cffb097cc54c36ed7eaf4276a
Andreas Weggel <andreas.weggel@gmail.com>
2018-03-08 15:12:02 +0100

    slug instead of str in path variable

--- ants/urls.py
+++ ants/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<str:slug>/', views.AntSpeciesDetail.as_view(), name='ant_detail')
+    path('<slug:slug>/', views.AntSpeciesDetail.as_view(), name='ant_detail')
 ]
