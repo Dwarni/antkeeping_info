3f5df7f20e5ddb14cd5e9a64e968126ca559b9fc
Andreas Weggel <andreas.weggel@gmail.com>
2018-03-09 14:03:54 +0100

    Both Country and Region now inherit from BaseRegion + increased code length

--- regions/models.py
+++ regions/models.py
@@ -3,30 +3,32 @@ from django.utils.translation import ugettext as _
 
 
 # Create your models here.
-class Country(models.Model):
+class BaseRegion(models.Model):
     code = models.CharField(
-        max_length=3,
-        verbose_name='Country code',
+        max_length=8,
+        verbose_name='ISO Code',
         db_index=True
     )
     name = models.CharField(max_length=200)
     ant_list_complete = models.BooleanField(default=False)
 
-    class Meta:
-        verbose_name = _('Country')
-        verbose_name_plural = _('Countries')
-        ordering = ['name']
-
     def __str__(self):
         return self.name
 
+    class Meta:
+        abstract = True
+        ordering = ['name']
 
-class Region(models.Model):
-    name = models.CharField(max_length=200)
+
+class Country(BaseRegion):
+    class Meta:
+        verbose_name = _('Country')
+        verbose_name_plural = _('Countries')
+
+
+class Region(BaseRegion):
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
     type = models.CharField(max_length=100)
-    code = models.CharField(max_length=20, db_index=True)
-    ant_list_complete = models.BooleanField(default=False)
 
     class Meta:
         verbose_name = _('Region')
