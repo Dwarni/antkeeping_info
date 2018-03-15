312eeb2eb2b2dbb861cda839196a159320ca2b7b
Andreas Weggel <andreas.weggel@gmail.com>
2018-03-15 18:24:39 +0100

    Added comments

--- regions/models.py
+++ regions/models.py
@@ -4,6 +4,7 @@ from django.utils.translation import ugettext as _
 
 # Create your models here.
 class BaseRegion(models.Model):
+    """Base region class"""
     code = models.CharField(
         max_length=8,
         verbose_name='ISO Code',
@@ -21,12 +22,19 @@ class BaseRegion(models.Model):
 
 
 class Country(BaseRegion):
+    """Represents a country"""
     class Meta:
         verbose_name = _('Country')
         verbose_name_plural = _('Countries')
 
 
 class Region(BaseRegion):
+    """Represents a Region within a country.
+
+    Can be a state, canton (Switzerland), department (France) etc.
+
+    Each region always belongs to one country.
+    """
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
     type = models.CharField(max_length=100)
 
