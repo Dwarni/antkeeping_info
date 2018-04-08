85ee26aaaeebf0c3398119e7128b7d13f985530a
Andreas Weggel <andreas.weggel@gmail.com>
2018-04-08 05:49:47 +0200

    Refactoring, Doctstrings

--- ants/views.py
+++ ants/views.py
@@ -1,5 +1,7 @@
-from decimal import Decimal
-from django.shortcuts import render, get_object_or_404, reverse
+"""
+    Module for all models of ants app.
+"""
+from django.shortcuts import get_object_or_404, reverse
 
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
@@ -7,15 +9,17 @@ from django.views.generic.base import TemplateView
 
 from django.db.models import Q
 
-from .models import AntRegion, AntSize, AntSpecies, CommonName, InvalidName
+from .models import AntRegion, AntSize, AntSpecies, InvalidName
 
 
 # Create your views here.
-class HomeView:
-    pass
 
 
 def add_iframe_to_context(context, request):
+    """
+        Get's the 'iframe'-parameter and sets the 'iframe'
+        key in the context dictionary accordingly.
+    """
     iframe = request.GET.get('iframe', None)
     if iframe and iframe == 'true':
         context['iframe'] = True
@@ -24,16 +28,20 @@ def add_iframe_to_context(context, request):
 
 
 class AntList(ListView):
+    """
+        Generic view for all lists of ants.
+    """
     context_object_name = 'ants'
     template_name = 'ants/ants_by_country/list.html'
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs): # pylint: disable=W0221
         context = super().get_context_data(**kwargs)
         add_iframe_to_context(context, self.request)
         return context
 
 
 class CountryIndex(TemplateView):
+    """Template view which get's a list of all countries with ants."""
     template_name = 'ants/ants_by_country/list.html'
 
     def get_context_data(self, **kwargs):
@@ -44,21 +52,34 @@ class CountryIndex(TemplateView):
 
 
 class CountryAntList(AntList):
-    def get_country(self):
-        self.country_code = self.kwargs['country_code']
-        self.country = get_object_or_404(AntRegion, code=self.country_code)
+    """
+        List view of ants of specific country.
+        The view also queries a list of all regions of that country
+        and in which ants occur.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.country = None
+
+    def set_country(self):
+        """
+            Sets the country according to the 'country_code'
+            parameter.
+        """
+        country_code = self.kwargs['country_code']
+        self.country = get_object_or_404(AntRegion, code=country_code)
 
     def get_queryset(self):
-        self.get_country()
-        return AntSpecies.objects.by_country(self.country_code)
+        self.set_country()
+        return AntSpecies.objects.by_country(self.country.code)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['country_code'] = self.country_code
+        context['country_code'] = self.country.code
         context['country'] = self.country
         context['ant_list_complete'] = self.country.ant_list_complete
         context['countries'] = AntRegion.countries.with_ants()
-        regions = AntRegion.states.with_ants_and_country(self.country_code)
+        regions = AntRegion.states.with_ants_and_country(self.country.code)
         context['regions'] = regions
 
         if regions.count() > 0:
@@ -74,40 +95,60 @@ class CountryAntList(AntList):
 
 
 class RegionAntList(CountryAntList):
+    """List view of ants of specific region."""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.region = None
+
+    def set_region(self):
+        """
+            Sets the region object according to the 'region_code'
+            parameter.
+        """
+        region_code = self.kwargs['region_code']
+        self.region = get_object_or_404(AntRegion, code=region_code)
+
     def get_queryset(self):
-        self.region_code = self.kwargs['region_code']
-        self.region = get_object_or_404(AntRegion, code=self.region_code)
-        self.get_country()
+        self.set_region()
+        self.set_country()
         query_set = AntSpecies.objects.by_region(
-            code=self.region_code
+            code=self.region.code
         )
         return query_set
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['region_code'] = self.region_code
+        context['region_code'] = self.region.code
         context['region'] = self.region
         context['ant_list_complete'] = self.region.ant_list_complete
         return context
 
 
-class AntSpeciesSearch(TemplateView):
-    template_name = 'ants/antspecies_list.html'
+class AntSpeciesSearch(AntList):
+    """Search view for ant species"""
+    def __init__(self):
+        self.search_item = None
+
+    def get_queryset(self):
+        query_set = None
+        self.search_item = self.request.GET.get('search_item')
+
+        if self.search_item:
+            query_set = AntSpecies.objects.filter(
+                Q(name__icontains=self.search_item) |
+                Q(commonname__name__icontains=self.search_item) |
+                Q(invalidname__name__icontains=self.search_item)).distinct()
+
+        return query_set
 
     def get_context_data(self, **kwargs):
-        context = super(AntSpeciesSearch, self).get_context_data(**kwargs)
-        search_term = self.request.GET.get('search_term')
-        if search_term:
-            object_list = AntSpecies.objects.filter(
-                Q(name__icontains=search_term) |
-                Q(commonname__name__icontains=search_term) |
-                Q(invalidname__name__icontains=search_term)).distinct()
-            context['object_list'] = object_list
-        context['search_term'] = search_term
+        context = super().get_context_data(**kwargs)
+        context['search_term'] = self.search_item
         return context
 
 
 class AntSpeciesDetail(DetailView):
+    """Detail view of an ant species."""
     model = AntSpecies
     template_name = 'ants/antspecies_detail/antspecies_detail.html'
 
@@ -119,7 +160,7 @@ class AntSpeciesDetail(DetailView):
         countries = AntRegion.countries.filter(distribution__species=ant.id)
         regions = {}
 
-        for country in countries:
+        for country in countries: # pylint: disable=E1133
             region_query = AntRegion.objects \
                 .filter(parent__code=country.code) \
                 .filter(distribution__species__id=ant.id)
@@ -142,47 +183,5 @@ class AntSpeciesDetail(DetailView):
         context['worker_size'] = worker_size
         context['queen_size'] = queen_size
         context['male_size'] = male_size
-
-        context
-        return context
-
-
-class NuptialFlight(TemplateView):
-    template_name = 'ants/nuptial_flight.html'
-
-    def get_context_data(self, **kwargs):
-        context = super(NuptialFlight, self).get_context_data(**kwargs)
-        ants = AntSpecies.objects.filter(nuptial_flight_start__isnull=False) \
-            .filter(nuptial_flight_end__isnull=False)
-
-        search_name = self.request.GET.get('name')
-        context['search_name'] = search_name
-
-        selected_country = kwargs['country'].lower()
-        if not selected_country == 'ALL':
-            ants = ants.filter(countries__code=selected_country)
-            regions = Region.objects.filter(country__code=selected_country) \
-                .filter(species__in=ants).distinct()
-            context['regions'] = regions
-            if('region' in kwargs):
-                selected_region = kwargs['region'].upper()
-                if not selected_region == 'ALL':
-                    ants = ants \
-                            .filter(regions__code=selected_region) \
-                            .distinct()
-                    context['region_code'] = selected_region
-
-        if search_name:
-            ants = ants.filter(
-                Q(name__icontains=search_name) |
-                Q(commonname__name__icontains=search_name) |
-                Q(oldname__name__icontains=search_name)).distinct()
-
-        countries = Country.objects.filter(
-            species__antspecies__nuptial_flight_start__isnull="False"
-            ).distinct()
-        context['object_list'] = ants
-        context['countries'] = countries
-        context['country_code'] = kwargs['country']
 
         return context
