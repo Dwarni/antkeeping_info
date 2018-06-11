c96735616904b8dd637b629e2256ef57ce9a7f1b
Andreas Weggel <andreas.weggel@gmail.com>
2018-06-11 20:33:52 +0200

    Added search input on map

--- flights/static/flights/js/flights_map.js
+++ flights/static/flights/js/flights_map.js
@@ -1,6 +1,7 @@
 var map = null;
 var markers = [];
 var flights = [];
+var filteredFlights = [];
 var mc = null; // Marker Clusterer
 var oms = null;
 var infoWindow = null;
@@ -9,6 +10,7 @@ var mapSettings = {
     center: {lat: 25, lng: 0},
     zoom: 2
 };
+var searchInput = null;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -29,6 +31,8 @@ function initMap() {
         anchor: new google.maps.Point(8, 8)
     }
 
+    searchInput = document.getElementById('antSearchInput');
+
     // get flights list
     updateMap();
 }
@@ -46,45 +50,71 @@ function updateMap() {
     markers = [];
     flights = [];
     $.getJSON( "/flights/list?year=" + year, function(data) {
-        oms.removeAllMarkers();
-        for(var i = 0; i < data.length; i++) {
-            var flight = data[i]
-            flights.push(flight)
-        // Create a marker for each place.
-            var newMarker = new google.maps.Marker({
-                map: map,
-                icon: markerIcon,
-                title: flight.ant,
-                position: {lat: flight.lat, lng: flight.lng}
-            });
-      
-            markers.push(newMarker);
-            newMarker.addListener('click', function() {
-                var that = this;
-                var markerIndex = markers.indexOf(this);
-                $.get(flights[markerIndex].id + '/info-window', function(data) {
-                    $('#flightInfoModalContent').html(data);
-                    $('#flightInfoModal').modal('toggle');
-                })
-            });
-            
-            oms.addMarker(newMarker);
-        }
-        if(mc) {
-            mc.clearMarkers();
-            mc.addMarkers(markers);
-        } else {
-            mc = new MarkerClusterer(map, markers, mcOptions);
-        }
-
-        // map.setCenter(mapSettings.center);
-        // map.setZoom(mapSettings.zoom);
+        flights = data;
+        filterFlights();
+        updateMarkers();
     })
+}
+
+function filterFlights() {
+    var filterString = searchInput.value;
+    var filterStringLower = filterString.toLowerCase();
+
+    if(filterString) {
+        filteredFlights = flights.filter(function(flight) {
+            var antSpeciesLower = flight.ant.toLowerCase();
+            return antSpeciesLower.indexOf(filterStringLower) >= 0;
+        })
+    } else {
+        filteredFlights = flights;
+    }
+}
+
+function updateMarkers() {
+    var mcOptions = {gridSize: 35, maxZoom: 8, imagePath: '/static/flights/img/vendor/markerclusterer/'};
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+        marker.setMap(null);
+    });
+    markers = [];
+    oms.removeAllMarkers();
+
+    for(var flight of filteredFlights) {
+        var newMarker = new google.maps.Marker({
+            map: map,
+            icon: markerIcon,
+            title: flight.ant,
+            position: {lat: flight.lat, lng: flight.lng}
+        });
+  
+        markers.push(newMarker);
+        newMarker.addListener('click', function() {
+            var that = this;
+            var markerIndex = markers.indexOf(this);
+            $.get(filteredFlights[markerIndex].id + '/info-window', function(data) {
+                $('#flightInfoModalContent').html(data);
+                $('#flightInfoModal').modal('toggle');
+            })
+        });
+        
+        oms.addMarker(newMarker);
+    }
+
+    if(mc) {
+        mc.clearMarkers();
+        mc.addMarkers(markers);
+    } else {
+        mc = new MarkerClusterer(map, markers, mcOptions);
+    }   
 }
 
 window.onload = function() {
     var yearSelect = document.getElementById('yearSelect');
     yearSelect.onchange = function() {
         updateMap();
-    }    
+    }
+    searchInput.onkeyup = function() {
+        filterFlights();
+        updateMarkers();
+    }
 }
