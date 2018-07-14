6b7459d05081e08d1a08739857e97788fadaacdd
Andreas Weggel <andreas.weggel@gmail.com>
2018-07-14 22:50:50 +0200

    updated settings

--- antkeeping_info/settings_dev.py
+++ antkeeping_info/settings_dev.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'dal',
+    'dal_select2',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -48,6 +50,9 @@ INSTALLED_APPS = [
     'django_bootstrap_breadcrumbs',
     'snowpenguin.django.recaptcha2',
     'cookielaw',
+    'taggit',
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
