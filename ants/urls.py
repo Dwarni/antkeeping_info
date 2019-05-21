0a68eed9e037967652c9cbfa25885a00c09a024e
Andreas Weggel <andreas.weggel@gmail.com>
2019-05-21 19:05:45 +0200

    formatting

--- ants/urls.py
+++ ants/urls.py
@@ -6,7 +6,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('ant-species-autocomplete/', 
+    path('ant-species-autocomplete/',
          views.AntSpeciesAutocomplete.as_view(),
          name='ant_species_autocomplete'),
     path('<slug:slug>/', views.AntSpeciesDetail.as_view(), name='ant_detail')
