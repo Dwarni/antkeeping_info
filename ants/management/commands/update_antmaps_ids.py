49f0dc93dfd96720aee0792f5ad1557c98fb88b9
Andreas Weggel <andreas.weggel@gmail.com>
2019-07-27 10:58:02 +0000

    Added command to add/update all antmaps_ids to regions

---
+++ ants/management/commands/update_antmaps_ids.py
@@ -0,0 +1,13 @@
+from django.core.management import BaseCommand
+
+from ants.services.antmaps import update_antmaps_ids
+
+
+class Command(BaseCommand):
+    """
+        The command updates adds/updates all antmaps region ids.
+    """
+    help = 'Add/Update antmaps.org region ids.'
+
+    def handle(self, *args, **options):
+        update_antmaps_ids()
