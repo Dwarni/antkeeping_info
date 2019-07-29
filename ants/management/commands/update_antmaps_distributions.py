61740be233f446052386e2b46bed702b701338a9
Andreas Weggel <andreas.weggel@gmail.com>
2019-07-29 21:08:06 +0000

    added command to update antmaps distributions

---
+++ ants/management/commands/update_antmaps_distributions.py
@@ -0,0 +1,15 @@
+from django.core.management import BaseCommand
+
+from ants.services.antmaps import update_species_of_regions
+
+
+class Command(BaseCommand):
+    """
+        The command adds/updates all distributions according to antmaps.
+        Currently the command won't remove any species from regions they
+        are not native to according to antmaps.
+    """
+    help = 'Add/Update antmaps.org distribution'
+
+    def handle(self, *args, **options):
+        update_species_of_regions()
