5e59e4034585a421d88108c0ff391fa9e977aab9
Andreas Weggel <andreas.weggel@gmail.com>
2021-03-25 06:07:19 +0100

    don't cache home

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.views.decorators.cache import never_cache
 
 
-# Create your views here.
+@never_cache
 def home(request):
     return render(request, "home/home.html")
