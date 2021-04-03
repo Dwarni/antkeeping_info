6778e65f8bda49a368579ce4c34c58c58bee4d7b
Andreas Weggel <andreas.weggel@gmail.com>
2021-04-03 07:22:12 +0200

    reduced speed and changed easing method

--- global_static/js/layout.js
+++ global_static/js/layout.js
@@ -9,5 +9,5 @@ $(window).scroll(function() {
 $('#return-to-top').click(function() {      // When arrow is clicked
     $('body,html').animate({
         scrollTop : 0                       // Scroll to top of body
-    }, 500);
+    }, 100, 'linear');
 });
