347da65db953cde065b59f0bc829c254d552e81b
Andreas Weggel <andreas.weggel@gmail.com>
2021-04-13 14:06:49 +0200

    changed view name

--- ants/antdb_urls.py
+++ ants/antdb_urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('ants-by-country', views.AntsByCountry.as_view(),
-         name='ants_by_country'),
+    path('ants-by-country', views.AntsByRegion.as_view(),
+         name='ant_species_by_country'),
 ]
