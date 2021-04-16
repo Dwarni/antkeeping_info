830f12a1ac33c567ae67f1a600d2c583bd341c51
Andreas Weggel <andreas.weggel@gmail.com>
2021-04-16 12:49:56 +0200

    improved pagination tag

--- bootstrap_tags/templatetags/bootstraptags.py
+++ bootstrap_tags/templatetags/bootstraptags.py
@@ -2,6 +2,8 @@ from django import template
 
 register = template.Library()
 
+counter = 0
+
 
 @register.simple_tag
 def url_replace(request, field, value):
@@ -12,10 +14,12 @@ def url_replace(request, field, value):
 
 @register.inclusion_tag("bootstrap_tags/pagination.html")
 def bs_pagination(page, request, **kwargs):
-    pages_before_after = 5
+    pages_before_after = 3
     current_page = page.number
     pages_before = min(current_page - 1, pages_before_after)
-    pages_after = min(page.paginator.num_pages - current_page, pages_before_after)
+    pages_after = min(page.paginator.num_pages - current_page,
+                      pages_before_after)
     start_page = current_page - pages_before
     end_page = current_page + pages_after
-    return {'page': page, 'page_range': range(start_page, end_page + 1), 'request': request}
+    return {'page': page, 'page_range': range(start_page, end_page + 1),
+            'request': request}
