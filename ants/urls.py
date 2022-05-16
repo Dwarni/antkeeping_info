f2d3d3362b19040413b7b7b03d12af6e59f6a954
Andreas Weggel <andreas.weggel@gmail.com>
2022-05-16 09:22:20 +0200

    make case insensitive

--- ants/urls.py
+++ ants/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('ant-species-autocomplete/',
          views.AntSpeciesAutocomplete.as_view(),
          name='ant_species_autocomplete'),
-    path('<slug:slug>/', views.AntSpeciesDetail.as_view(), name='ant_detail'),
+    path('<str:slug>/', views.AntSpeciesDetail.as_view(), name='ant_detail'),
 ]
