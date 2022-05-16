0c5c5d1ce132309809f2dea0b2c23d8693f84c84
Andreas Weggel <andreas.weggel@gmail.com>
2022-05-16 09:45:40 +0200

    created case insensitive slug converter

---
+++ ants/converters.py
@@ -0,0 +1,8 @@
+class CaseInsensitiveSlugConverter:
+    regex = '\\w+-\\w+'
+
+    def to_python(self, value):
+        return value.lower()
+
+    def to_url(self, value):
+        return value
--- ants/urls.py
+++ ants/urls.py
@@ -1,13 +1,15 @@
 """
     url module for ants app
 """
-from django.urls import path
+from django.urls import path, register_converter
 
-from . import views
+from . import converters, views
+
+register_converter(converters.CaseInsensitiveSlugConverter, 'islug')
 
 urlpatterns = [
     path('ant-species-autocomplete/',
          views.AntSpeciesAutocomplete.as_view(),
          name='ant_species_autocomplete'),
-    path('<str:slug>/', views.AntSpeciesDetail.as_view(), name='ant_detail'),
+    path('<islug:slug>/', views.AntSpeciesDetail.as_view(), name='ant_detail'),
 ]
