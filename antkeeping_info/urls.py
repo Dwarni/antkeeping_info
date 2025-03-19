4ac5e69386bf3073cb6f2785c4ccbe259bec0c04
Andreas Weggel <andreas.weggel@gmail.com>
2025-03-19 13:15:43 +0100

    added static files to urlpatterns if run in DEBUG mode

--- antkeeping_info/urls.py
+++ antkeeping_info/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from debug_toolbar.toolbar import debug_toolbar_urls
 
 from django.urls import include, path
@@ -57,6 +58,7 @@ if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT)
+    urlpatterns += staticfiles_urlpatterns()
 
 
 if settings.DEBUG and not settings.TESTING:
