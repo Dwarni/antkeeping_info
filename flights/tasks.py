ea93f81f8140e213eb56d9cf8e2eeb59a9b9883c
Andreas Weggel <andreas.weggel@gmail.com>
2025-03-19 10:23:52 +0100

    it is now called shared_task

--- flights/tasks.py
+++ flights/tasks.py
@@ -1,11 +1,11 @@
 # Create your tasks here
 from __future__ import absolute_import, unicode_literals
-from celery import task
+from celery import shared_task
 from celery.utils.log import get_task_logger
 
 logger = get_task_logger(__name__)
 
 
-@task
+@shared_task
 def hello_world():
     logger.info('Hello world!')
