045961e2627bc84bf7ab8e5a497a63c7052be6df
Andreas Weggel <andreas.weggel@gmail.com>
2026-03-02 19:14:55 +0100

    build: added django-extensions for manage.py

--- antkeeping_info/settings.py
+++ antkeeping_info/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.sitemaps",
+    "django_extensions",
     "ants",
     "home",
     "contact",
